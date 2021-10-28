1d71c2722371e26ed00cd51f0a5b0e1c9557f5ee
Elijah <elijahmylcev@gmail.com>
2021-10-28 21:56:39 +0300

    Added hamburger

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.scss';
+
+const hamburger = document.querySelector('.hamburger')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active')
+})
