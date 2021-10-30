7c68d6e5f38447cc693232abb17611482f3ee950
Elijah <elijahmylcev@gmail.com>
2021-10-30 20:31:30 +0300

    menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import './style.scss';
 
 const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('.menu')
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger-active')
+    hamburger.classList.toggle('hamburger-active');
+    menu.classList.toggle('menu-active');
+
+    if (menu.classList.contains('menu-active')) {
+        
+    }
 })
