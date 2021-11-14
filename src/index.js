824fdd5ed68f2b832407170ded727b029a4595d1
Elijah <elijahmylcev@gmail.com>
2021-11-14 12:33:42 +0300

    getting all items & addEventListener

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './style.scss';
 
-const hamburger = document.querySelector('.hamburger')
-const menu = document.querySelector('.menu')
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const menuItem = document.querySelectorAll('.menu__item');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('hamburger-active');
   menu.classList.toggle('menu-active');
+});
 
-})
+menuItem.forEach(element => {
+  element.addEventListener('click', () => {
+    console.log('open');
+  })
+});
+console.log(menuItem);
