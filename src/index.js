6a4c4387c815cf791e7cd4a3c3bd51f08ef925f4
Elijah <elijahmylcev@gmail.com>
2021-11-14 14:31:32 +0300

    refact code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.scss';
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const menuItem = document.querySelectorAll('.menu__item_title');
+const Gal = document.querySelectorAll('.menu__item_title_wrapper')
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('hamburger-active');
@@ -11,6 +12,17 @@ hamburger.addEventListener('click', () => {
 
 menuItem.forEach(element => {
   element.addEventListener('click', (event) => {
-    event.target.closest('.menu__item').lastElementChild.classList.toggle('body-opened')
+
+    menuItem.forEach(element => {
+      if (element.closest('.menu__item').lastElementChild.classList.contains('body-opened')) {
+        element.closest('.menu__item').lastElementChild.classList.remove('body-opened');
+
+        element.closest('.menu__item').firstElementChild.lastElementChild.style.transform = 'none';
+      }
+    });
+
+    event.target.closest('.menu__item').lastElementChild.classList.toggle('body-opened');
+
+    event.target.closest('.menu__item').firstElementChild.lastElementChild.style.transform = 'translate(4px) rotate(180deg)';
   })
 });
