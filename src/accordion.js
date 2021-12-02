cb91a2e9e888d3f59722ca96283aca7040d09047
Elijah <elijahmylcev@gmail.com>
2021-12-02 20:17:39 +0300

    working function for accordion

--- src/accordion.js
+++ src/accordion.js
@@ -17,7 +17,15 @@ function toggleAccordionSection(triggersSelector, itemsSelector) {
 
   buttons.forEach((btn) => {
     btn.addEventListener('click', function () {
+      // Индикатор выбранного заголовка
       this.lastElementChild.classList.toggle('title-active');
+      this.nextElementSibling.classList.toggle('body-active');
+
+      if (this.lastElementChild.classList.contains('title-active')) {
+        this.nextElementSibling.style.maxHeight = `${this.nextElementSibling.scrollHeight}px`;
+      } else {
+        this.nextElementSibling.style.maxHeight = '0px';
+      }
     });
   });
 }
