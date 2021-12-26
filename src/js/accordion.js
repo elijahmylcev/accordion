f73b8ea4e61bd8d7550f9a3a99a96cd33059833a
Elijah Mylcev <elijahmylcev@gmail.com>
2021-12-26 15:15:36 +0300

    Rewrite

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -3,19 +3,21 @@ import delegate from './delegate';
 const accordion = (triggerSelector) => {
   const triggers = document.querySelectorAll(triggerSelector);
 
-  delegate({
-    triggers: triggers,
-    event: 'click',
-    condition: (target) => target.className === 'accordion__title',
-    cb: (trigger) => {
-      trigger.lastElementChild.classList.toggle('title-active');
-      trigger.nextElementSibling.classList.toggle('body-active');
-      if (trigger.lastElementChild.classList.contains('title-active')) {
-        trigger.nextElementSibling.style.maxHeight = `${trigger.nextElementSibling.scrollHeight}px`;
-      } else {
-        trigger.nextElementSibling.style.maxHeight = '0px';
-      }
-    },
+  triggers.forEach((trigger) => {
+    delegate({
+      trigger: trigger,
+      event: 'click',
+      condition: (target) => target.className === 'accordion__title',
+      delegate: (trigger) => {
+        trigger.lastElementChild.classList.toggle('title-active');
+        trigger.nextElementSibling.classList.toggle('body-active');
+        if (trigger.lastElementChild.classList.contains('title-active')) {
+          trigger.nextElementSibling.style.maxHeight = `${trigger.nextElementSibling.scrollHeight}px`;
+        } else {
+          trigger.nextElementSibling.style.maxHeight = '0px';
+        }
+      },
+    });
   });
 };
 
--- src/js/delegate.js
+++ src/js/delegate.js
@@ -1,11 +1,9 @@
-const delegate = ({ triggers, event, condition, cb }) => {
-  triggers.forEach((el) => {
-    el.addEventListener(event, (e) => {
-      const target = e.target;
-      if (condition(target)) {
-        cb(el);
-      }
-    });
+const delegate = ({ trigger, event, condition, delegate }) => {
+  trigger.addEventListener(event, (e) => {
+    const target = e.target;
+    if (condition(target)) {
+      delegate(trigger);
+    }
   });
 };
 
